5df169b457c84ec043c4f63225b86c2ccd5f552e
HarisSpahija <haris@DESKTOP-NPEUG46>
2017-12-05 16:03:58 +0100

    Removed test by u/StefanPater

--- app/src/main/java/nl2017_2018tle12_ond_t1/hr/cmi/project/tle2_app/MainActivity.java
+++ app/src/main/java/nl2017_2018tle12_ond_t1/hr/cmi/project/tle2_app/MainActivity.java
@@ -2,14 +2,12 @@ package nl2017_2018tle12_ond_t1.hr.cmi.project.tle2_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("TEST", "onCreate: TEST");
         setContentView(R.layout.activity_main);
     }
 }
